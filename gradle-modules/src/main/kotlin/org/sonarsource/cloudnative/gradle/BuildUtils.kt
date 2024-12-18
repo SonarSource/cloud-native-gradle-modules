5b09d9a85301d155657aa1c40123943a033d9c84
Peter Trifanov <peter.trifanov@sonarsource.com>
2024-12-18 15:57:09 +0100

    SONARGO-72 Add Gradle module for rule-api update (#11)

--- gradle-modules/src/main/kotlin/org/sonarsource/cloudnative/gradle/BuildUtils.kt
+++ gradle-modules/src/main/kotlin/org/sonarsource/cloudnative/gradle/BuildUtils.kt
@@ -17,9 +17,38 @@
 package org.sonarsource.cloudnative.gradle
 
 import org.gradle.api.Project
+import org.gradle.api.artifacts.dsl.RepositoryHandler
+import org.gradle.api.artifacts.repositories.MavenArtifactRepository
+import org.gradle.api.internal.file.FileOperations
+import org.gradle.api.provider.ProviderFactory
 
 fun Project.signingCondition(): Boolean {
     val branch = System.getenv()["CIRRUS_BRANCH"] ?: ""
     return (branch == "master" || branch.matches("branch-[\\d.]+".toRegex())) &&
         gradle.taskGraph.hasTask(":artifactoryPublish")
 }
+
+internal fun RepositoryHandler.repox(
+    repository: String,
+    providers: ProviderFactory,
+    fileOperations: FileOperations,
+): MavenArtifactRepository =
+    maven {
+        name = "artifactory"
+        url = fileOperations.uri("https://repox.jfrog.io/repox/$repository")
+
+        // This authentication relies on env vars configured on Cirrus CI or on Gradle properties (`-P<prop>` flags or `gradle.properties` file)
+        val artifactoryUsername = providers.environmentVariable("ARTIFACTORY_PRIVATE_USERNAME")
+            .orElse(providers.gradleProperty("artifactoryUsername"))
+        val artifactoryPassword = providers.environmentVariable("ARTIFACTORY_PRIVATE_PASSWORD")
+            .orElse(providers.gradleProperty("artifactoryPassword"))
+
+        if (artifactoryUsername.isPresent && artifactoryPassword.isPresent) {
+            authentication {
+                credentials {
+                    username = artifactoryUsername.get()
+                    password = artifactoryPassword.get()
+                }
+            }
+        }
+    }
