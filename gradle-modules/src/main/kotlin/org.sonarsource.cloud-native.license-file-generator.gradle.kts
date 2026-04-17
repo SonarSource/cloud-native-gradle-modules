b962cbffc4825835e1fd192e6f7d236e56d2b18a
Peter Trifanov <peter.trifanov@sonarsource.com>
2026-04-17 11:28:29 +0200

    CLOUDSEC-11 Add base plugin to exclude BouncyCastle from build classpath (#124)
    Workaround for a Gradle issue: when a
    cloud-native Gradle plugin is applied in a project whose Gradle version
    embeds Kotlin <2.3.20, an unnecessary BouncyCastle dependency appears on
    the build classpath. The new `base` plugin clears it, and all other
    plugins now apply it.

--- gradle-modules/src/main/kotlin/org.sonarsource.cloud-native.license-file-generator.gradle.kts
+++ gradle-modules/src/main/kotlin/org.sonarsource.cloud-native.license-file-generator.gradle.kts
@@ -24,6 +24,7 @@ import org.sonarsource.cloudnative.gradle.areFilesEqual
 import org.sonarsource.cloudnative.gradle.copyDirectory
 
 plugins {
+    id("org.sonarsource.cloud-native.base")
     id("com.github.jk1.dependency-license-report")
 }
 
