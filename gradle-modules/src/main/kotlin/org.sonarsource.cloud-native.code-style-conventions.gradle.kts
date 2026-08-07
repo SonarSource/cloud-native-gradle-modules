fb3312c40793f71d3a26d8f41d97c30b93d58b18
Marcin Stachniuk <marcin.stachniuk@sonarsource.com>
2026-08-07 14:17:44 +0200

    SONARIAC-2308 Migrate codebase from JSR305 to JSpecify

--- gradle-modules/src/main/kotlin/org.sonarsource.cloud-native.code-style-conventions.gradle.kts
+++ gradle-modules/src/main/kotlin/org.sonarsource.cloud-native.code-style-conventions.gradle.kts
@@ -60,7 +60,7 @@ spotless {
     }
     format("javaMisc") {
         target("src/**/package-info.java")
-        licenseHeaderFile(licenseHeaderFile, "")
+        licenseHeaderFile(licenseHeaderFile, "(@javax.annotation|@org.jspecify)")
     }
 }
 
